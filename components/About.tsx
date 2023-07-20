a09fcc6047d6fd99a4a9cf77b5495ae11fcf498c
hvanjerdz <hvanjerdz@proton.me>
2023-07-19 23:20:40 -0600

    Fix compile error (escaping  with '&#39;')

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className="text-lg tracking-wide flex flex-col gap-6">
         <p>
           I am a Mexican computer science student with an interest in free software, electronics, 
-          backend development, DevOps, unix-like systems, databases and someone else's computer.
+          backend development, DevOps, unix-like systems, databases and someone else&#39;s computer.
         </p>
         <p>
           Feel free to contact me. I am always eager to learn about better practices and new stuff.
